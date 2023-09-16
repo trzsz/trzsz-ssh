290dc9484e042fcaef23d7711f2c44a41e779061
Lonny Wong <lonnywong@qq.com>
2023-09-16 13:03:32 +0800

    add copyright

--- tssh/term_unix.go
+++ tssh/term_unix.go
@@ -4,6 +4,7 @@
 MIT License
 
 Copyright (c) 2023 Lonny Wong <lonnywong@qq.com>
+Copyright (c) 2023 [Contributors](https://github.com/trzsz/trzsz-ssh/graphs/contributors)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
