892f53b9da0a51ac1e549460236bf02559abf7b5
fa1seu <178009183+fa1seut0pia@users.noreply.github.com>
2024-10-19 20:39:34 +0800

    refactor: dns support, add TestDNS

---
+++ tssh/dns_test.go
@@ -0,0 +1,48 @@
+package tssh
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDNS(t *testing.T) {
+
+	assert := assert.New(t)
+	assertDestEqual := func(preParseDns, dnsResolution string) {
+
+		t.Helper()
+		network, dns, err := resolveDnsAddress(preParseDns)
+		assert.Nil(err)
+		assert.Equal(fmt.Sprintf("%s://%s", network, dns), dnsResolution)
+
+	}
+
+	assertDestNotNil := func(preParseDns string) {
+		t.Helper()
+		_, _, err := resolveDnsAddress(preParseDns)
+		assert.NotNil(err)
+
+	}
+
+	assertDestNotNil("ab cd")
+	assertDestNotNil("udp://ab:cd")
+
+	assertDestEqual("8.8.8.8", "udp://8.8.8.8:53")
+	assertDestEqual("8.8.8.8:53", "udp://8.8.8.8:53")
+	assertDestEqual("udp://8.8.8.8", "udp://8.8.8.8:53")
+	assertDestEqual("udp://8.8.8.8:53", "udp://8.8.8.8:53")
+	assertDestEqual("tcp://8.8.8.8", "tcp://8.8.8.8:53")
+	assertDestEqual("tcp://8.8.8.8:53", "tcp://8.8.8.8:53")
+	assertDestEqual("udp://8.8.8.8:5300", "udp://8.8.8.8:5300")
+
+	assertDestEqual("2001:4860:4860::8888", "udp://[2001:4860:4860::8888]:53")
+	assertDestEqual("[2001:4860:4860::8888]:53", "udp://[2001:4860:4860::8888]:53")
+	assertDestEqual("udp://2001:4860:4860::8888", "udp://[2001:4860:4860::8888]:53")
+	assertDestEqual("udp://[2001:4860:4860::8888]:53", "udp://[2001:4860:4860::8888]:53")
+	assertDestEqual("tcp://2001:4860:4860::8888", "tcp://[2001:4860:4860::8888]:53")
+	assertDestEqual("tcp://[2001:4860:4860::8888]:53", "tcp://[2001:4860:4860::8888]:53")
+	assertDestEqual("udp://[2001:4860:4860::8888]:5300", "udp://[2001:4860:4860::8888]:5300")
+
+}
