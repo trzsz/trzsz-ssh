791b68c585273a8465b4ef6d7805ca513cb76832
Lonny Wong <lonnywong@qq.com>
2025-10-12 22:15:50 +0800

    update copyright

--- tssh/agent_unix.go
+++ tssh/agent_unix.go
@@ -3,7 +3,7 @@
 /*
 MIT License
 
-Copyright (c) 2023-2024 The Trzsz SSH Authors.
+Copyright (c) 2023-2025 The Trzsz SSH Authors.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
